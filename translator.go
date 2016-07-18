0f0a10b6db8f2ee4579c38c323876c986a9e26b6
Adria Cidre <adria.cidre@gmail.com>
2016-07-18 12:29:51 +0200

    [#ERNEST-1854] : AWS instance adapter

--- translator.go
+++ translator.go
@@ -15,31 +15,34 @@ type disk struct {
 }
 
 type builderEvent struct {
-	Uuid               string `json:"_uuid"`
-	BatchID            string `json:"_batch_id"`
-	Type               string `json:"type"`
-	Service            string `json:"service"`
-	Name               string `json:"name"`
-	CPU                int    `json:"cpus"`
-	RAM                int    `json:"ram"`
-	IP                 string `json:"ip"`
-	Catalog            string `json:"reference_catalog"`
-	Image              string `json:"reference_image"`
-	Disks              []disk `json:"disks"`
-	RouterName         string `json:"router_name"`
-	RouterType         string `json:"router_type"`
-	RouterIP           string `json:"router_ip"`
-	ClientName         string `json:"client_name"`
-	DatacenterName     string `json:"datacenter_name"`
-	DatacenterPassword string `json:"datacenter_password"`
-	DatacenterRegion   string `json:"datacenter_region"`
-	DatacenterType     string `json:"datacenter_type"`
-	DatacenterUsername string `json:"datacenter_username"`
-	NetworkName        string `json:"network_name"`
-	VCloudURL          string `json:"vcloud_url"`
-	Status             string `json:"status"`
-	ErrorCode          string `json:"error_code"`
-	ErrorMessage       string `json:"error_message"`
+	Uuid                  string   `json:"_uuid"`
+	BatchID               string   `json:"_batch_id"`
+	Type                  string   `json:"type"`
+	Service               string   `json:"service"`
+	Name                  string   `json:"name"`
+	CPU                   int      `json:"cpus"`
+	RAM                   int      `json:"ram"`
+	IP                    string   `json:"ip"`
+	Catalog               string   `json:"reference_catalog"`
+	Image                 string   `json:"reference_image"`
+	Disks                 []disk   `json:"disks"`
+	RouterName            string   `json:"router_name"`
+	RouterType            string   `json:"router_type"`
+	RouterIP              string   `json:"router_ip"`
+	ClientName            string   `json:"client_name"`
+	DatacenterName        string   `json:"datacenter_name"`
+	DatacenterPassword    string   `json:"datacenter_password"`
+	DatacenterRegion      string   `json:"datacenter_region"`
+	DatacenterType        string   `json:"datacenter_type"`
+	DatacenterUsername    string   `json:"datacenter_username"`
+	DatacenterAccessToken string   `json:"datacenter_token"`
+	DatacenterAccessKey   string   `json:"datacenter_secret"`
+	NetworkName           string   `json:"network_name"`
+	SecurityGroupAWSIDs   []string `json:"security_group_aws_ids"`
+	VCloudURL             string   `json:"vcloud_url"`
+	Status                string   `json:"status"`
+	ErrorCode             string   `json:"error_code"`
+	ErrorMessage          string   `json:"error_message"`
 }
 
 type instanceResource struct {
@@ -76,23 +79,21 @@ type vcloudEvent struct {
 }
 
 type awsEvent struct {
-	Uuid    string `json:"_uuid"`
-	BatchID string `json:"_batch_id"`
-	Type    string `json:"_type"`
-	// TODO: Map correct fields
-	/*
-		DatacenterRegion      string `json:"datacenter_region,omitempty"`
-		DatacenterAccessToken string `json:"datacenter_access_token,omitempty"`
-		DatacenterAccessKey   string `json:"datacenter_access_key,omitempty"`
-		DatacenterVpcID       string `json:"datacenter_vpc_id,omitempty"`
-		NetworkSubnet         string `json:"network_subnet"`
-		DatacenterName        string `json:"datacenter_name,omitempty"`
-		DatacenterUsername    string `json:"datacenter_username,omitempty"`
-		DatacenterPassword    string `json:"datacenter_password,omitempty"`
-	*/
-	Status       string `json:"status"`
-	ErrorCode    string `json:"error_code"`
-	ErrorMessage string `json:"error_message"`
+	Uuid                  string   `json:"_uuid"`
+	BatchID               string   `json:"_batch_id"`
+	Type                  string   `json:"_type"`
+	DatacenterRegion      string   `json:"datacenter_region,omitempty"`
+	DatacenterAccessToken string   `json:"datacenter_access_token"`
+	DatacenterAccessKey   string   `json:"datacenter_access_key"`
+	DatacenterVpcID       string   `json:"datacenter_vpc_id,omitempty"`
+	NetworkAWSID          string   `json:"network_aws_id"`
+	SecurityGroupAWSIDs   []string `json:"security_group_aws_ids"`
+	InstanceName          string   `json:"instance_name"`
+	InstanceImage         string   `json:"instance_image"`
+	InstanceType          string   `json:"instance_type"`
+	Status                string   `json:"status"`
+	ErrorCode             string   `json:"error_code"`
+	ErrorMessage          string   `json:"error_message"`
 }
 
 type Translator struct{}
@@ -103,9 +104,9 @@ func (t Translator) BuilderToConnector(j []byte) []byte {
 	json.Unmarshal(j, &input)
 
 	switch input.DatacenterType {
-	case "vcloud", "vcloud_fake", "fake":
+	case "vcloud", "fake-vcloud", "fake":
 		output = t.builderToVCloudConnector(input)
-	case "aws", "aws_fake":
+	case "aws", "fake-aws":
 		output = t.builderToAwsConnector(input)
 	}
 
@@ -142,6 +143,9 @@ func (t Translator) builderToVCloudConnector(input builderEvent) []byte {
 	output.DatacenterPassword = input.DatacenterPassword
 	output.DatacenterType = input.DatacenterType
 	output.VCloudURL = input.VCloudURL
+	output.Status = input.Status
+	output.ErrorCode = input.ErrorCode
+	output.ErrorMessage = input.ErrorMessage
 
 	body, _ := json.Marshal(output)
 
@@ -153,15 +157,20 @@ func (t Translator) builderToAwsConnector(input builderEvent) []byte {
 
 	output.Uuid = input.Uuid
 	output.BatchID = input.BatchID
-	/*
-		output.Service = input.Service
-		output.Type = input.RouterType
-		output.DatacenterRegion = input.DatacenterRegion
-		output.DatacenterAccessToken = input.DatacenterAccessToken
-		output.DatacenterAccessKey = input.DatacenterAccessKey
-		output.DatacenterVpcID = input.DatacenterName
-		output.NetworkSubnet = input.NetworkSubnet
-	*/
+	output.Type = input.DatacenterType
+	output.DatacenterRegion = input.DatacenterRegion
+	output.DatacenterAccessToken = input.DatacenterAccessToken
+	output.DatacenterAccessKey = input.DatacenterAccessKey
+	output.DatacenterVpcID = input.DatacenterName
+	output.NetworkAWSID = input.NetworkName
+	output.SecurityGroupAWSIDs = input.SecurityGroupAWSIDs
+	// TODO: Documentation says something about Private IPS, but can't find any specs about it
+	output.InstanceName = input.Name
+	output.InstanceImage = input.Image
+	output.InstanceType = input.Type
+	output.Status = input.Status
+	output.ErrorCode = input.ErrorCode
+	output.ErrorMessage = input.ErrorMessage
 
 	body, _ := json.Marshal(output)
 
@@ -175,10 +184,10 @@ func (t Translator) ConnectorToBuilder(j []byte) []byte {
 	dec := json.NewDecoder(bytes.NewReader(j))
 	dec.Decode(&input)
 
-	switch input["datacenter_type"] {
-	case "vcloud", "vcloud_fake", "fake":
+	switch input["_type"] {
+	case "vcloud", "fake-vcloud", "fake":
 		output = t.vcloudConnectorToBuilder(j)
-	case "aws", "aws_fake":
+	case "aws", "fake-aws":
 		output = t.awsConnectorToBuilder(j)
 	}
 
@@ -211,6 +220,9 @@ func (t Translator) vcloudConnectorToBuilder(j []byte) []byte {
 	output.DatacenterPassword = input.DatacenterPassword
 	output.DatacenterType = input.DatacenterType
 	output.VCloudURL = input.VCloudURL
+	output.Status = input.Status
+	output.ErrorCode = input.ErrorCode
+	output.ErrorMessage = input.ErrorMessage
 
 	body, _ := json.Marshal(output)
 
@@ -225,13 +237,20 @@ func (t Translator) awsConnectorToBuilder(j []byte) []byte {
 	output.Uuid = input.Uuid
 	output.BatchID = input.BatchID
 	output.Type = input.Type
-	/*
-		output.DatacenterRegion = input.DatacenterRegion
-		output.DatacenterAccessToken = input.DatacenterAccessToken
-		output.DatacenterAccessKey = input.DatacenterAccessKey
-		output.DatacenterName = input.DatacenterVpcID
-		output.NetworkSubnet = input.NetworkSubnet
-	*/
+	output.DatacenterRegion = input.DatacenterRegion
+	output.DatacenterAccessToken = input.DatacenterAccessToken
+	output.DatacenterAccessKey = input.DatacenterAccessKey
+	output.DatacenterName = input.DatacenterVpcID
+	output.NetworkName = input.NetworkAWSID
+	output.SecurityGroupAWSIDs = input.SecurityGroupAWSIDs
+	// TODO: Documentation says something about Private IPS, but can't find any specs about it
+	output.Name = input.InstanceName
+	output.Image = input.InstanceImage
+	output.Type = input.InstanceType
+	output.Status = input.Status
+	output.ErrorCode = input.ErrorCode
+	output.ErrorMessage = input.ErrorMessage
+
 	body, _ := json.Marshal(output)
 
 	return body
